76c7464c285d6eb48c90980b80bfeec5a42d5106
jrzollin <jrzollin@yahoo.com>
2017-05-25 21:28:35 -0700

    updated question 1.  Finshed question 2.

--- app.js
+++ app.js
@@ -18,23 +18,59 @@ var question1 = prompt('Am I a native to Seattle?').toLowerCase();
 
 if(question1 === 'yes'){
   console.log('user has input yes for question 1');
+  alert('Nope!  I moved here in 2007.');
 } else if(question1 === 'no'){
   console.log('user has input no for question 1');
+  alert('Correct!  I moved here in 2007.');
 } else {
-  alert('That is not a valid response.  \'yes\' or \'no\' only please!')
-  console.log('user has input not input a valid answer for question 1');
-  var question1 = prompt('Am I a native to Seattle?');
+  alert('That is not a valid response.  \'yes\' or \'no\' only please!');
+  console.log('user has not input a valid answer for question 1');
+  var question1 = prompt('Am I a native to Seattle?').toLowerCase();
   if(question1 === 'yes'){
     console.log('user has input yes for question 1');
+    alert('Nope! I moved here in 2007.');
   } else if(question1 === 'no'){
-    console.log('user has input no for question 1')
+    console.log('user has input no for question 1');
+    alert('Correct!  I moved here in 2007.');
   } else {
-    alert('Again, that is invalid.  Okay, I\'m putting your answer as \'no\'.');
-    console.log('user has again input an invalid answer for question 1.  answer is now no');
+    alert('Again, that is invalid.  Okay, I\'m putting your answer as \'yes\'.  That is incorrect dummy.');
+    console.log('user has again input an invalid answer for question 1.  answer is now incorrect');
   }
 }
 
 var question2 = prompt('Have I served in the military?').toLowerCase();
+
+if(question2 === 'yes'){
+  console.log('user has input yes for question 2');
+  alert('Correct!  I served in the Air Force for four years.');
+} else if (question2 === 'no'){
+  console.log('user has input no for question 2');
+  alert('Wrong!  I served in the Air Force for four years.');
+} else {
+  alert('That is not a valid response. \'yes\' or \'no\' only please!');
+  console.log('user has not input a valid answer for question 2');
+  var question2 = prompt('Have I served in the military?').toLowerCase();
+  if(question2 === 'yes'){
+    console.log('user has input yes for question 2');
+    alert('Correct!  I served in the Air Force for four years.');
+  } else if(question2 === 'no') {
+    console.log('user has input no for question 2');
+    alert('Wrong!  I served in the Air Force for four years.');
+  } else {
+    alert('Again, that is invalid.  Okay, I\'m putting your answer as \'no\'.  Wrong!');
+    console.log('user has again input an invalid answer for question 2.  answer is now incorrect');
+  }
+}
+
 var question3 = prompt('Do I have pets?').toLowerCase();
+
+if(question3 === 'yes'){
+  console.log('user has input yes for question 3');
+} else if(question3 === 'no'){
+  console.log('user has input no for question 3');
+} else {
+
+}
+
 var question4 = prompt('Is my favorite color green?').toLowerCase();
 var question5 = prompt('Can I play an instrument?').toLowerCase();
